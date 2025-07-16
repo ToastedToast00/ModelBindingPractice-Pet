c5abecb42ef2f5ea4d10f3200b63f73e4a05e961
Toasty <toastedtoast0090@gmail.com>
2025-07-16 15:45:53 -0700

    Added custom error messages to pet.cs and started data validations to controller

--- Models/pet.cs
+++ Models/pet.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelBindingPractice_Pet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingPractice_Pet.Models
 {
     /// <summary>
     /// this class represents a pet, Id is the primary key
@@ -6,8 +8,13 @@
     public class Pet
     {
         public int Id { get; set; }
+        [StringLength(20)]
+        [Required(ErrorMessage = "Enter valid name")]
         public required string Name { get; set; }
+        [StringLength(50)]
+        [Required(ErrorMessage = "Enter valid species")]
         public required string Species { get; set; }
+        [Range(0, 25)] //Assume age in years, 0-25 is a reasonable range for pets
         public int Age { get; set; }
         public bool IsFixed { get; set; }
     }
