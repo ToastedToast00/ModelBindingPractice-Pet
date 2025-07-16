00d914c04c7e6b3ab6a9682e2f9277ad55c5595c
Toasty <toastedtoast0090@gmail.com>
2025-07-16 14:51:43 -0700

    Made Controller and cshtml page for creation of pet in database

---
+++ Controllers/PetController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingPractice_Pet.Controllers;
+
+public class PetController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
+
