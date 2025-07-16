e8b7a5b71bf7655fc4f458a66f04737b23846b9c
Toasty <toastedtoast0090@gmail.com>
2025-07-16 14:39:31 -0700

    added required on all string types in pet.cs to force valid input

--- Models/pet.cs
+++ Models/pet.cs
@@ -6,8 +6,8 @@
     public class Pet
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Species { get; set; }
+        public required string Name { get; set; }
+        public required string Species { get; set; }
         public int Age { get; set; }
         public bool IsFixed { get; set; }
     }
